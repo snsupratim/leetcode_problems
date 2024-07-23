25d466dd28ed14d4688235bcdef6fe33c6dab541
Supratim Nag <138885948+snsupratim@users.noreply.github.com>
2024-07-23 09:21:55 +0530

    Update ContainsDuplicateII.java

--- array/ContainsDuplicateII.java
+++ array/ContainsDuplicateII.java
@@ -1,1 +1,18 @@
+// Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array
+//   such that nums[i] == nums[j] and abs(i - j) <= k..
 
+class ContainsDuplicatesII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++) {
+            int current = nums[i];
+            if(map.containsKey(current) && i - map.get(current) <= k) {
+                return true;
+            } else {
+                map.put(current, i);
+            }
+        }
+        
+        return false;
+    }
+}
